43e50a3370764a7b44178dfad3995122b0fedea8
hansent <thomas.hansen@gmail.com>
2014-02-20 12:20:16 -0600

    clean up index.js formatting and add some comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,67 @@
+
 $(document).ready(function(){
   bootbox.prompt("What is your name?", join_chat);
 });
 
+
 function join_chat(nickname){
+  
+  /***********************************************
+   * initialize socket connection for chat
+   **********************************************/
+  
+  //if no nickname was given...kepp asking instead
   if (!nickname)
     return bootbox.prompt("You must choose a nickname?", join_chat);
 
+  //connect using socket.io
   write_output('>> connecting...');
   var socket = io.connect('/');
+
+
+  /***********************************************
+   * chat protocol / socket.io event handlers
+   **********************************************/
   socket.on('welcome', on_welcome);
   socket.on('new_message', on_new_message);
-
-  function write_output(text){
-    var new_msg = $("<p>"+text+"</p>");
-    $("#output").append(new_msg);
-  }
-
+ 
+  //server sends a welcome when we connect successfully
   function on_welcome(event_data){
     write_output('>> connected successfully');
     write_output('>> welcome '+nickname);
   }
 
+  //new chat message from the server
   function on_new_message(event_data){
     write_output(event_data.text);
   }
+  
+  // write a line of outut to the chat window
+  function write_output(text){
+    var new_msg = $("<p>"+text+"</p>");
+    $("#output").append(new_msg);
+  }
 
+  //send a message with the text enetred by the user
+  function send_message(){
+    //get the text enetered in teh input field
+    var input_el = $("#input");
+    var input_text = input_el.val();
+
+    //send message to server
+    socket.emit('message', {nick: nickname, text: input_text});
+
+    //reset input field
+    input_el.val('');
+  }
+
+
+  /**********************************************
+   * DOM event handlers
+   **********************************************/
   $("#send_button").click( function(ev){
-    var input_text = $("#input").val();
-    socket.emit('message', {text: input_text});
-    $("#input").val('');
     ev.preventDefault();
-    console.log(input_text);
+    send_message();
   });
 }
 
